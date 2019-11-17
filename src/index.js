5e8555b55c49e1e79d03a0b8e507f3a2de886896
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2019-11-17 09:51:17 +0100

    ✨ Add new link subscription

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
+import { split } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
 import { createHttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
@@ -10,7 +13,6 @@ import App from './components/App'
 import { AUTH_TOKEN } from './constants'
 import * as serviceWorker from './serviceWorker'
 import './styles/index.css'
-
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
@@ -26,8 +28,27 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
